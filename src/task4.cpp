ef2a92c77c6eea5cfb00e772bae56fd749cdc1e6
Sergey <baranenkovs@mail.ru>
2019-10-09 14:40:06 +0300

    fixed bug with tmp

--- src/task4.cpp
+++ src/task4.cpp
@@ -1,16 +1,18 @@
 #include "task4.h"
-#include <iostream>
 #include <string.h>
+#include <new>
+#include <stdio.h>
 using namespace std;
 
-char *strreverse(char *str){
+char *strreverse(char *str)
+{
     if (!str || ! *str)
         return str;
 
     int i = strlen(str) - 1, j = 0;
-
     char ch;
-    while (i > j){
+    while (i > j)
+    {
         ch = str[i];
         str[i] = str[j];
         str[j] = ch;
@@ -20,37 +22,42 @@ char *strreverse(char *str){
     return str;
 }
 
-char * sum(char *x, char *y){
+char * sum(char *x, char *y)
+{
 
-    unsigned long i = 0,
-                  xLen = strlen(x),
-                  yLen = strlen(y),
-                  maxL = ((xLen>=yLen)?xLen:yLen);
-    unsigned short tmp,
-                   sum = 0;
+    unsigned long i = 0, xLen = strlen(x), yLen = strlen(y), maxL = ((xLen>=yLen)?xLen:yLen);
+    unsigned short tmp = 0, sum = 0;
 
-    char *res = (char*)malloc((maxL+2) * sizeof(char));
+    char *res = new char(maxL+2);
 
-    while(i<yLen||i<xLen){
-        if ((i<xLen)&&(i<yLen)){
-          sum = *(x+xLen-i-1)-'0'+ *(y+yLen-i-1)-'0' +tmp;
-        }else if(i<xLen){
-          sum = *(x+xLen-i-1) - '0' + tmp;
-        }else{
-          sum = *(y+yLen-i-1) - '0' + tmp;
+    while(i<yLen||i<xLen)
+    {
+        if ((i<xLen)&&(i<yLen))
+        {
+            sum = *(x+xLen-i-1)-'0' + *(y+yLen-i-1)-'0' + tmp;
+        }
+        else if(i<xLen)
+        {
+            sum = *(x+xLen-i-1) - '0' + tmp;
+        }
+        else
+        {
+            sum = *(y+yLen-i-1) - '0' + tmp;
         }
 
-        tmp = int(sum/10);
-        *(res+i)=(sum%10)+'0';
+        tmp = int(sum / 10);
+        *(res+i)=(sum % 10) + '0';
         i++;
     }
 
-    if(tmp){
-        *(res+i)=tmp+'0';
+    if(tmp)
+    {
+        *(res+i) = tmp + '0';
         i++;
     }
 
-    *(res+i)='\0';
+    *(res+i) = '\0';
     strreverse(res);
     return res;
 }
+
