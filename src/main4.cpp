1655466e7a10231067f0fedf3c6bdf455a587c2e
Sergey <baranenkovs@mail.ru>
2019-12-11 09:56:55 +0300

    Have no idea

--- src/main4.cpp
+++ src/main4.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "task4.h"
 using namespace std;
-int main()
-{
-    char* num1 = "1111111";
-    char* num2 = "39111111";
-    cout<<sum(num1,num2)<<'\n';
+int main(){
+    char a[] = "99999999999999999999";
+    char b[] = "1";
+    std::cout << sum(a, b);
     return 0;
 }
 
